01457c9a9f7c3e0be3c08f59e19f5f0febad3428
AbmSourav <keramotul.islam@gmail.com>
2019-01-09 21:24:37 +0600

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-App
-Simple App with React JS
---
+++ precache-manifest.e51e27d0ca9cc2d4e2bac6ef19b58395.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "abfeebd436016ce1da09",
+    "url": "/React-App/static/js/runtime~main.abfeebd4.js"
+  },
+  {
+    "revision": "0cf363c6bdde06e05664",
+    "url": "/React-App/static/js/main.0cf363c6.chunk.js"
+  },
+  {
+    "revision": "b18dc658627f62f6f72f",
+    "url": "/React-App/static/js/1.b18dc658.chunk.js"
+  },
+  {
+    "revision": "0cf363c6bdde06e05664",
+    "url": "/React-App/static/css/main.9996e7f3.chunk.css"
+  },
+  {
+    "revision": "ec84ed615687c96000ac7c55b1d3829b",
+    "url": "/React-App/index.html"
+  }
+];
